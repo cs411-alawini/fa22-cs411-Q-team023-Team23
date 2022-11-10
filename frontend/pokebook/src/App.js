431b249014c8d22558805b2bd274d037b804e429
jialinh4 <jialinh4@illinois.edu>
2022-11-10 17:14:15 -0600

    changed app.js

--- frontend/pokebook/src/App.js
+++ frontend/pokebook/src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>411 Project 1 Stage 4 CRUD Implementation</h1>
+
+      <div className = "form">
+        <label>Id: </label>
+        <input type = "text" name = "UserId"></input>
+        <label>Name: </label>
+        <input type = "text" name = "UserName"></input>
+        <label>Email: </label>
+        <input type = "text" name = "UserEmail"></input>
+        <label>Password: </label>
+        <input type = "text" name = "UserPassword"></input>
+        <button>Submit</button>
+
+      </div>
     </div>
   );
 }
