b4457b46575c35aa1fb37aa907eeaa204f232569
jinyuy2 <jinyuy2@illinois.edu>
2022-11-12 03:59:05 -0600

    advanced1&2

--- frontend/pokebook/src/App.js
+++ frontend/pokebook/src/App.js
@@ -14,6 +14,12 @@ function App() {
   const [searchResult, setSearchResult] = useState('');
   const [userList, setUserList] = useState([]);
 
+  const [advance1Result, setadvance1Result] = useState('');
+  const [advance1List, setadvance1List] = useState([]);
+
+  const [advance2Result, setadvance2Result] = useState('');
+  const [advance2List, setadvance2List] = useState([]);
+
 
   useEffect(() => {
     Axios.get('https://localhost:3002/api/get').then((response) => {
@@ -56,11 +62,36 @@ function App() {
     });
   };
 
-  const overallSearch = () => {
-    Axios.get('http://localhost:3002/api/search/all').then((response) => {
-      setUserList(response.data)
+  const submitAdvance1 = () => {
+    Axios.post('http://localhost:3002/api/advance1', {
+    }).then((response) => {
+      if (response.data.length > 0) {
+        setadvance1List(response.data);
+        setadvance1Result('Here is your result!\n');
+      } else {
+        setadvance1Result('No records according to your conditions!\n');
+      }
     });
   };
+
+  const submitAdvance2 = () => {
+    Axios.post('http://localhost:3002/api/advance2', {
+    }).then((response) => {
+      if (response.data.length > 0) {
+        setadvance2List(response.data);
+        setadvance2Result('Here is your result!\n');
+      } else {
+        setadvance2Result('No records according to your conditions!\n');
+      }
+    });
+  };
+
+
+  // const overallSearch = () => {
+  //   Axios.get('http://localhost:3002/api/search/all').then((response) => {
+  //     setUserList(response.data)
+  //   });
+  // };
 
 
   return ( 
@@ -118,7 +149,7 @@ function App() {
         <div>{searchResult}</div>
         {userList.map((val) => {
           return (
-          <div className='SearchResult'> 
+          <div className='SearchResult'>  
             <h3>UserId: {val.UserId}</h3> 
             <h3>UserName: {val.UserName}</h3>
             <h3>UserEmail: {val.UserEmail}</h3> 
@@ -129,11 +160,41 @@ function App() {
 
         {/* AD 1 */}
         <br></br><label> AD 1 </label>
-        <button> Advanced Query 1</button>
+        <button onClick={submitAdvance1}> Submit Advance1 </button>
+        <div>{advance1Result}</div>
+        {advance1List.map((val) => {
+          return (
+          <div className='Advance2Result'> 
+            <h3>TypeName: {val.TypeName}</h3> 
+            <h3>PokemonName: {val.PokemonName}</h3>
+            <h3>Attack: {val.Attack}</h3> 
+            <h3>AvgAttack: {val.AvgAttack}</h3> 
+            <h3>Defense: {val.Defense}</h3> 
+            <h3>AvgDefense: {val.AvgDefense}</h3> 
+          </div>
+          );
+        })}
 
         {/* AD 2 */}
         <br></br><label> AD 2 </label>
-        <button> Advanced Query 2</button>
+        <button onClick={submitAdvance2}> Submit Advance2 </button>
+        <div>{advance2Result}</div>
+        {advance2List.map((val) => {
+          return (
+          <div className='Advance2Result'> 
+            <h3>PokemonName: {val.PokemonName}</h3> 
+            <h3>FirstTypeName: {val.FirstTypeName}</h3>
+            <h3>SecondTypeName: {val.SecondTypeName}</h3> 
+            <h3>Hp: {val.Hp}</h3> 
+            <h3>Attack: {val.Attack}</h3> 
+            <h3>Defense: {val.Defense}</h3> 
+            <h3>SpecialAttack: {val.SpecialAttack}</h3> 
+            <h3>SpecialDefense: {val.SpecialDefense}</h3> 
+            <h3>Speed: {val.Speed}</h3> 
+            <h3>Generation: {val.Generation}</h3> 
+          </div>
+          );
+        })}
 
         </div> )
 }
