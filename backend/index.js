0d7911011c561b46626eb81b42b7bdfa127d557b
jinyuy2 <80532956+jinyuy2@users.noreply.github.com>
2022-11-12 04:34:25 -0600

    stage4 completed

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/api/search/all", (req, res) => {
+app.get("/api/get", (req, res) => {
     const sqlSelect = "SELECT * FROM User";
     db.query(sqlSelect, (err, result) => {
         res.send(result);
@@ -24,16 +24,77 @@ app.get("/api/search/all", (req, res) => {
     });
 });
 
-app.get("/api/search/keyword?keyword=' + userName", (req, res) => {
-    const sqlSelect = "SELECT * FROM Users WHERE UserName = " + req.params.userName;
-    db.query(sqlSelect, (err, result) => {
+app.post("/api/search", (req, res) => {
+    const UserName = req.body.searchuserName;
+    // console.log('searching');
+    db.query('SELECT * FROM User WHERE UserName = ?', [UserName],
+    (err, result) => {
+        if (err) throw err;
         res.send(result);
         console.log(result);
-        if (err) throw err;
     });
 });
 
+app.post("/api/advance1search", (req, res) => {
+    // console.log('searching');
+    db.query(
+        "SELECT t1.TypeName, p1.PokemonName, p1.Attack, \
+        (SELECT AVG(p2.Attack)\
+                    FROM (Pokemon p2 LEFT JOIN PokemonType pt2 on p2.PokemonId = pt2.PokemonId) JOIN Type t2 ON (pt2.FirstTypeId = t2.TypeId)\
+        Where t1.TypeId = t2.TypeId\
+        GROUP BY t2.TypeId) as AvgAttack, \
+        p1.Defense, \
+        (SELECT AVG(p2.Defense)\
+                    FROM (Pokemon p2 LEFT JOIN PokemonType pt2 on p2.PokemonId = pt2.PokemonId) JOIN Type t2 ON (pt2.FirstTypeId = t2.TypeId)\
+        Where t1.TypeId = t2.TypeId\
+        GROUP BY t2.TypeId) as AvgDefense\
+    FROM (Pokemon p1 LEFT JOIN PokemonType pt1 on p1.PokemonId = pt1.PokemonId) JOIN Type t1 ON (pt1.FirstTypeId = t1.TypeId)\
+    WHERE p1.Attack > (SELECT AVG(p2.Attack)\
+                    FROM (Pokemon p2 LEFT JOIN PokemonType pt2 on p2.PokemonId = pt2.PokemonId) JOIN Type t2 ON (pt2.FirstTypeId = t2.TypeId)\
+        Where t1.TypeId = t2.TypeId\
+        GROUP BY t2.TypeId)\
+        AND    \
+    p1.Defense > (SELECT AVG(p2.Defense)\
+                    FROM (Pokemon p2 LEFT JOIN PokemonType pt2 on p2.PokemonId = pt2.PokemonId) JOIN Type t2 ON (pt2.FirstTypeId = t2.TypeId)\
+        Where t1.TypeId = t2.TypeId\
+        GROUP BY t2.TypeId)\
+    GROUP BY t1.TypeId, t1.TypeName, p1.PokemonName, p1.Attack, p1.Defense\
+    ORDER BY t1.TypeName, p1.PokemonName\
+    Limit 5", [],
+    (err, result) => {
+        if (err) throw err;
+        res.send(result);
+        console.log(result);
+    });
+});
 
+app.post("/api/advance2search", (req, res) => {
+    // console.log('searching');
+    db.query("(SELECT p2.PokemonName, t1.TypeName AS FirstTypeName, t2.TypeName AS SecondTypeName, p2.Hp, p2.Attack, p2.Defense, p2.SpecialAttack, p2.SpecialDefense, p2.Speed, p2.Generation\
+        FROM (Pokemon p2 LEFT JOIN PokemonType pt2 on p2.PokemonId = pt2.PokemonId) JOIN Type t1 ON (pt2.FirstTypeId = t1.TypeId) JOIN Type t2 ON (pt2.SecondTypeId = t2.TypeId)\
+        WHERE (t1.TypeName like '%Fire%' OR t2.TypeName like '%Fire%') AND p2.Generation >= 4\
+        ORDER BY p2.PokemonId ASC)\
+        \
+        UNION\
+        \
+        (SELECT p2.PokemonName, t1.TypeName AS FirstTypeName, t2.TypeName AS SecondTypeName, p2.Hp, p2.Attack, p2.Defense, p2.SpecialAttack, p2.SpecialDefense, p2.Speed, p2.Generation\
+        FROM (Pokemon p2 LEFT JOIN PokemonType pt2 on p2.PokemonId = pt2.PokemonId) JOIN Type t1 ON (pt2.FirstTypeId = t1.TypeId) JOIN Type t2 ON (pt2.SecondTypeId = t2.TypeId)\
+        WHERE (t1.TypeName like '%Water%' OR t2.TypeName like '%Water%') AND p2.Generation >= 4\
+        ORDER BY p2.PokemonId ASC)\
+        \
+        UNION\
+        \
+        (SELECT p2.PokemonName, t1.TypeName AS FirstTypeName, t2.TypeName AS SecondTypeName, p2.Hp, p2.Attack, p2.Defense, p2.SpecialAttack, p2.SpecialDefense, p2.Speed, p2.Generation\
+        FROM (Pokemon p2 LEFT JOIN PokemonType pt2 on p2.PokemonId = pt2.PokemonId) JOIN Type t1 ON (pt2.FirstTypeId = t1.TypeId) JOIN Type t2 ON (pt2.SecondTypeId = t2.TypeId)\
+        WHERE (t1.TypeName like '%Grass%' OR t2.TypeName like '%Grass%') AND p2.Generation >= 4\
+        ORDER BY p2.PokemonId ASC)\
+        LIMIT 5", [],
+    (err, result) => {
+        if (err) throw err;
+        res.send(result);
+        console.log(result);
+    });
+});
 
 app.post("/api/insert", (req, res) => {
     const InsertuserId = req.body.insertuserId;
