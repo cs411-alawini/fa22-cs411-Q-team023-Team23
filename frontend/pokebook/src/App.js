b32d63bc902e46a3f5779e8175cd4d63cd89e91b
jinyuy2 <80532956+jinyuy2@users.noreply.github.com>
2022-11-12 04:33:41 -0600

    stage4 completed

--- frontend/pokebook/src/App.js
+++ frontend/pokebook/src/App.js
@@ -10,10 +10,22 @@ function App() {
   const [deleteuserId, setDeleteUserId] = useState('');
   const [updateuserName, setUpdateUserName] = useState('');
   const [updateuserPassword, setUpdateUserPassword] = useState('');
-  const [userList, setUserList] = useState('');
+  const [searchuserName, setSearchUserName] = useState('');
+  const [searchResult, setSearchResult] = useState('');
+  const [userList, setUserList] = useState([]);
+
+  const [advance1Result, setadvance1Result] = useState('');
+  const [advance1List, setadvance1List] = useState([]);
+
+  const [advance2Result, setadvance2Result] = useState('');
+  const [advance2List, setadvance2List] = useState([]);
 
 
-
+  useEffect(() => {
+    Axios.get('https://localhost:3002/api/get').then((response) => {
+      setUserList(response.data)
+    })
+  },[])
 
   const submitInsert = () => { 
     Axios.post('http://localhost:3002/api/insert', {
@@ -37,122 +49,156 @@ function App() {
     });
   };
 
-  // const submitSearch = () => {
-  //   Axios.get('http://localhost:3002/api/search/keyword?keyword=' + userName).then((response) => {
-  //     setUserList(response.data)
-  //   }) 
+  const submitSearch = () => {
+    Axios.post('http://localhost:3002/api/search', {
+      searchuserName: searchuserName
+    }).then((response) => {
+      if (response.data.length > 0) {
+        setUserList(response.data);
+        setSearchResult('Here is your result!\n');
+      } else {
+        setSearchResult('No records according to your conditions!\n');
+      }
+    });
+  };
 
+  const submitAdvance1 = () => {
+    Axios.post('http://localhost:3002/api/advance1search', {
+    }).then((response) => {
+      if (response.data.length > 0) {
+        setadvance1List(response.data);
+        setadvance1Result('Here is your result!\n');
+      } else {
+        setadvance1Result('No records according to your conditions!\n');
+      }
+    });
+  };
+
+  const submitAdvance2 = () => {
+    Axios.post('http://localhost:3002/api/advance2search', {
+    }).then((response) => {
+      if (response.data.length > 0) {
+        setadvance2List(response.data);
+        setadvance2Result('Here is your result!\n');
+      } else {
+        setadvance2Result('No records according to your conditions!\n');
+      }
+    });
+  };
+
+
+  // const overallSearch = () => {
+  //   Axios.get('http://localhost:3002/api/search/all').then((response) => {
+  //     setUserList(response.data)
+  //   });
   // };
 
-  const overallSearch = () => {
-    Axios.get('http://localhost:3002/api/search/all').then((response) => {
-      setUserList(response.data)
-    });
 
+  return ( 
+        <div className="form">
+        <h1>CS411 CURD Functions</h1>
 
-
-  };
-  
-
-  // return (
-  //   <div className="App">
-  //     <h1> CRUD APPLICATIONS</h1>
-
-  //     <div className="form">
+        {/* Insert */}
+        <label> Insert </label>
+        <label> UserId: </label>
+        <input type="text" name="userId1" onChange={(e) => {
+          setInsertUserId(e.target.value)
+        }}/>
+        <label> UserName: </label>
+        <input type="text" name="userName1" onChange={(e) => {
+          setInsertUserName(e.target.value)
+        }}/>
+        <label> UserPassword:</label>
+        <input type="text" name="userPassword1" onChange={(e) => {
+          setInsertUserPassword(e.target.value)
+        }}/>
+        <label> UserEmail:</label>
+        <input type="text" name="userEmail1" onChange={(e) => {
+          setInsertUserEmail(e.target.value)
+        }}/>
+        <button onClick={submitInsert}> Submit Insert</button>
         
-  //       <label> Insert </label>
-  //       <label> UserName: </label>
-  //       <input type="text" name="userName1" onChange={(e) => {
-  //         setUserName(e.target.value)
-  //       } }/>
-  //       <label> UserPassword:</label>
-  //       <input type="text" name="userPassword1" onChange={(e) => {
-  //         setUserPassword(e.target.value)
-  //       }}/>
-  //       <label> UserEmail:</label>
-  //       <input type="text" name="userEmail1" onChange={(e) => {
-  //         setUserEmail(e.target.value)
-  //       }}/>
-  //       <button onClick={submitInsert}> Submit Insert</button>
+        {/* Delete */}
+        <br></br><label> Delete </label>
+        <label> UserId: </label>
+        <input type="text" name="userId4" onChange={(e) => {
+          setDeleteUserId(e.target.value)
+        }}/>
+        <button onClick={submitDelete}> Submit Delete</button>
 
-
-  //       <label> Search </label>
-  //       <label> UserName: </label>
-  //       <input type="text" name="userName2" onChange={(e) => {
-  //         setUserName(e.target.value)
-  //       } }/>
-  //       <button onClick={submitSearch}> Submit Search</button>
-
-
-
-
-  //       <label> Delete </label>
-  //       <label> UserName: </label>
-  //       <input type="text" name="userName4" onChange={(e) => {
-  //         setUserName(e.target.value)
-  //       } }/>
-  //       <button onClick={submitDelete}> Submit Delete</button>
-
-
-  //       <button> onClick={overallSearch} Overall Search </button>
-        
-  //       { userList.map((val) => {
-  //           return (
-  //             <div>
-  //               <p>UserName: {val.userName}</p>
-  //               <p>UserPassword: {val.userPassword}</p>
-  //               <p>UserEmail{val.userEmail}</p>
-                
-  //             </div>
-
-  //           )
-  //       })
-
-  //       }
-  //     </div>
-      
-  //   </div>
-  // );
-
-  return ( <div className="form">
-          <label> Insert </label>
-          <label> UserId: </label>
-          <input type="text" name="userId1" onChange={(e) => {
-           setInsertUserId(e.target.value)
-          } }/>
-          <label> UserName: </label>
-          <input type="text" name="userName1" onChange={(e) => {
-           setInsertUserName(e.target.value)
-          } }/>
-          <label> UserPassword:</label>
-          <input type="text" name="userPassword1" onChange={(e) => {
-           setInsertUserPassword(e.target.value)
-          }}/>
-          <label> UserEmail:</label>
-          <input type="text" name="userEmail1" onChange={(e) => {
-           setInsertUserEmail(e.target.value)
-          }}/>
-          <button onClick={submitInsert}> Submit Insert</button>
-          
-          <br></br><label> Delete </label>
-         <label> UserId: </label>
-         <input type="text" name="userId4" onChange={(e) => {
-           setDeleteUserId(e.target.value)
-         } }/>
-         <button onClick={submitDelete}> Submit Delete</button>
-
+        {/* Update */}
         <br></br><label> Update </label>
         <label> UserName: </label>
         <input type="text" name="userName3" onChange={(e) => {
           setUpdateUserName(e.target.value)
-        } }/>
+        }}/>
         <label> UserPassword:</label>
         <input type="text" name="userPassword3" onChange={(e) => {
           setUpdateUserPassword(e.target.value)
         }}/>
         <button onClick={submitUpdate}> Submit Update</button>
 
-          </div> )
+        {/* Search */}
+        <br></br><label> Search </label>
+        <label> UserName: </label>
+        <input type="text" name="userName2" onChange={(e) => {
+          setSearchUserName(e.target.value)
+        }}/>
+        <button onClick={submitSearch}> Submit Search</button>
+
+        <div>{searchResult}</div>
+        {userList.map((val) => {
+          return (
+          <div className='SearchResult'>  
+            <h3>UserId: {val.UserId}</h3> 
+            <h3>UserName: {val.UserName}</h3>
+            <h3>UserEmail: {val.UserEmail}</h3> 
+            <h3>UserPassword: {val.UserPassword}</h3> 
+          </div>
+          );
+        })}
+
+        {/* AD 1 */}
+        <br></br><label> AD 1 </label>
+        <button onClick={submitAdvance1}> Submit Advance1 </button>
+
+        <div>{advance1Result}</div>
+        {advance1List.map((val) => {
+          return (
+          <div className='Advance1Result'> 
+            <h3>TypeName: {val.TypeName}</h3> 
+            <h3>PokemonName: {val.PokemonName}</h3>
+            <h3>Attack: {val.Attack}</h3> 
+            <h3>AvgAttack: {val.AvgAttack}</h3> 
+            <h3>Defense: {val.Defense}</h3> 
+            <h3>AvgDefense: {val.AvgDefense}</h3> 
+          </div>
+          );
+        })}
+
+        {/* AD 2 */}
+        <br></br><label> AD 2 </label>
+        <button onClick={submitAdvance2}> Submit Advance2 </button>
+
+        <div>{advance2Result}</div>
+        {advance2List.map((val) => {
+          return (
+          <div className='Advance2Result'> 
+            <h3>PokemonName: {val.PokemonName}</h3> 
+            <h3>FirstTypeName: {val.FirstTypeName}</h3>
+            <h3>SecondTypeName: {val.SecondTypeName}</h3> 
+            <h3>Hp: {val.Hp}</h3> 
+            <h3>Attack: {val.Attack}</h3> 
+            <h3>Defense: {val.Defense}</h3> 
+            <h3>SpecialAttack: {val.SpecialAttack}</h3> 
+            <h3>SpecialDefense: {val.SpecialDefense}</h3> 
+            <h3>Speed: {val.Speed}</h3> 
+            <h3>Generation: {val.Generation}</h3> 
+          </div>
+          );
+        })}
+
+        </div> )
 }
 
 export default App;
